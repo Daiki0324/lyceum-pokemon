a1f159343ef343488f0261ff10778f86eded520e
Daiki Sato <daiki.sato.03@gmail.com>
2024-02-07 16:48:37 +0900

    ポケモンをつかまえる機能を実装

--- pages/trainer/[name]/index.vue
+++ pages/trainer/[name]/index.vue
@@ -6,8 +6,13 @@ const { data: trainer } = await useFetch(
   {
     default: () => [],
     baseUrl: config.public.backendOrigin,
-  },
+  }
 );
+const {
+  dialog: deleteDialog,
+  onOpen: onOpenDelete,
+  onClose: onCloseDelete,
+} = useDialog();
 </script>
 
 <template>
@@ -17,6 +22,41 @@ const { data: trainer } = await useFetch(
       <img src="/avatar.png" />
       <span>{{ trainer.name }}</span>
     </div>
+    <GamifyButton @click="onOpenDelete(true)"
+      >マサラタウンにかえる</GamifyButton
+    >
+    <h2>てもちポケモン</h2>
+    <CatchButton :to="`/trainer/${route.params.name}/catch`"
+      >ポケモンをつかまえる</CatchButton
+    >
+    <GamifyList>
+      <GamifyItem v-for="pokemon in trainer.pokemons" :key="pokemon.id">
+        <img :src="pokemon.sprites.front_default" />
+        <span class="pokemon-name">{{ pokemon.nickname || pokemon.name }}</span>
+        <GamifyButton @click="onOpenNickname(pokemon)"
+          >ニックネームをつける</GamifyButton
+        >
+        <GamifyButton @click="onOpenRelease(pokemon)"
+          >はかせにおくる</GamifyButton
+        >
+      </GamifyItem>
+    </GamifyList>
+    <GamifyDialog
+      v-if="deleteDialog"
+      id="confirm-delete"
+      title="かくにん"
+      description="ほんとうに　マサラタウンに　かえるんだな！　この　そうさは　とりけせないぞ！"
+      @close="onCloseDelete"
+    >
+      <GamifyList :border="false" direction="horizon">
+        <GamifyItem>
+          <GamifyButton @click="onCloseDelete">いいえ</GamifyButton>
+        </GamifyItem>
+        <GamifyItem>
+          <GamifyButton @click="onDelete">はい</GamifyButton>
+        </GamifyItem>
+      </GamifyList>
+    </GamifyDialog>
   </div>
 </template>
 
